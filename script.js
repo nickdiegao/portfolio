7c4229276ac8ebaaa7f2adef4e539e17172671a3
Nicholas Diego <nickdiegao@gmail.com>
2023-11-28 12:36:49 -0300

    Mexendo na responsividade

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 function alterarCor() {
-    let corFundo = '#F3A712';
     let cabecalho = document.querySelectorAll('.menu-link');
     let corTexto = document.querySelectorAll('.card-texto');
     let nomeAluno = document.querySelectorAll('.nome-aluno');
@@ -7,69 +6,84 @@ function alterarCor() {
     let corLinks = document.querySelectorAll('.card-link');
     let rodape = document.querySelectorAll('.rodape');
     let botao = document.querySelector('#botao')
-    
+
+    let modoAtual = localStorage.getItem('modo');
+
+    //dark mode
+    let corBodyD = '#1d1d1d'
+    let corCabecalhoD = '#ffffff'
+    let corNomesD = '#28a745'
+    let corTextosD = '#ffc107'
+    let corExtraD = '#9b59b6'
+
+    //white mode
+    let corBodyW = '#ffffff'
+    let corCabecalhoW = '#333333'
+    let corNomesW = '#1abc9c'
+    let corTextosW = '#ff802c'
+    let corExtraW = '#9b59b6'
+
     if (botao.value === 'DARK MODE') {
         botao.value = 'WHITE MODE';
-        botao.style.color = corFundo;
-        botao.style.backgroundColor = '#141414';
-        document.body.style.backgroundColor = corFundo
+        botao.style.color = corBodyD;
+        botao.style.backgroundColor = corNomesD;
+        document.body.style.backgroundColor = corBodyD
 
         cabecalho.forEach(function(cabecalho){
-            cabecalho.style.color = '#141414';
+            cabecalho.style.color = corCabecalhoD;
         })
         
         corTexto.forEach(function(corTexto){
-            corTexto.style.color = '#BB4430';
+            corTexto.style.color = corTextosD;
             // console.log('funciona')
         });
 
         nomeAluno.forEach(function(nomeAluno){
-            nomeAluno.style.color = '#EEF0F2';
+            nomeAluno.style.color = corNomesD;
         })
 
         tituloDesc.forEach(function(tituloDesc) {
-            tituloDesc.style.color = '#011638'; 
+            tituloDesc.style.color = corExtraD; 
         });
 
         corLinks.forEach(function(corLinks){
-            corLinks.style.color = '#BB4430';
+            corLinks.style.color = corTextosD;
         })
 
         rodape.forEach(function(rodape){
-            rodape.style.backgroundColor = '#141414'
-            rodape.style.color = '#F3A712'
+            rodape.style.backgroundColor = corExtraD
+            rodape.style.color = corBodyD
         })
 
       } else {
         botao.value = 'DARK MODE';
-        botao.style.color = '#EBE9E9';
-        botao.style.backgroundColor = '#3581B8';
-        document.body.style.backgroundColor = '#EBE9E9'
+        botao.style.color = corBodyW;
+        botao.style.backgroundColor = corNomesW;
+        document.body.style.backgroundColor = corBodyW
 
         cabecalho.forEach(function(cabecalho){
-            cabecalho.style.color = '#3581B8';
+            cabecalho.style.color = corCabecalhoW;
         })
 
         corTexto.forEach(function(corTexto){
-            corTexto.style.color = '#ff802c';
+            corTexto.style.color = corTextosW;
         });
 
         nomeAluno.forEach(function(nomeAluno){
-            nomeAluno.style.color = '#3581B8';
+            nomeAluno.style.color = corNomesW;
         })
 
         tituloDesc.forEach(function(tituloDesc) {
-            tituloDesc.style.color = '#3581B8'; 
+            tituloDesc.style.color = corExtraW; 
           });
 
         corLinks.forEach(function(corLinks){
-            corLinks.style.color = '#ff802c';
+            corLinks.style.color = corTextosW;
         })
 
         rodape.forEach(function(rodape){
-            rodape.style.backgroundColor = '#3581B8'
-            rodape.style.color = '#EBE9E9'
+            rodape.style.backgroundColor = corExtraW
+            rodape.style.color = corBodyW
         })
-
       }
 }
